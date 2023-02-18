476931562ffabb26b9b7cfefed464895c5681f91
Ilkhom82 <116688893+Ilkhom82@users.noreply.github.com>
2023-02-17 22:33:14 -0600

    fix secret

--- test/docker_secret.sh
+++ test/docker_secret.sh
@@ -1,6 +1,6 @@
-read -p "Please anter your ocker username: "  DOCKER_USERNAME
-read -p "Please anter your ocker password: " DOCKER_PASSWORD
-read -p "Please anter your ocker email: "    DOCKER_EMAIL
+read -p "Please enter your docker username: "  DOCKER_USERNAME
+read -p "Please enter your docker password: " DOCKER_PASSWORD
+read -p "Please enter your docker email: "    DOCKER_EMAIL
 
 kubectl create secret bocker-registry regcred --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL
 
