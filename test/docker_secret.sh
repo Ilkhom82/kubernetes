bb4573b16143a4e49bbd81f9c0e011f8b96c5144
Ilkhom82 <116688893+Ilkhom82@users.noreply.github.com>
2023-02-17 22:35:08 -0600

    fix secret1

--- test/docker_secret.sh
+++ test/docker_secret.sh
@@ -5,4 +5,4 @@ read -p "Please enter your docker email: "    DOCKER_EMAIL
 kubectl create secret bocker-registry regcred --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL
 
 # bash docker_secret.sh
-# kubectl get secret
+# kubectl get secret 
